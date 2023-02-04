bcbc3c7968475781f04a4616bc4e1b8f051e9da2
riadshuvo <riadshuvo55@gmail.com>
2023-02-04 15:57:22 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,161 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lecle_social_share/lecle_social_share.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? image;
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImageC() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Image Picker Example"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text("Pick Image from Gallery",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold)),
+                  onPressed: () {
+                    pickImage();
+                  }),
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text("Pick Image from Camera",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold)),
+                  onPressed: () {
+                    pickImageC();
+                  }),
+              SizedBox(
+                height: 20,
+              ),
+              image != null ? Image.file(image!) : Text("No image selected"),
+              ElevatedButton(
+                onPressed: () {
+                  // await appinioSocialShare.shareToFacebook('', image!.path);
+
+                  LecleSocialShare.F.shareFileToFacebook(
+                    filePath: image!.path,
+                    fileProviderPath: '',
+                    fileType: AssetType.image,
+                  );
+                },
+                child: const Text('Share media content file to facebook'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  LecleSocialShare.W.shareFileToWhatsApp(
+                    filePath: image!.path,
+                    fileProviderPath: '',
+                    fileType: AssetType.image,
+                  );
+                },
+                child: const Text('Share media content file to Whatsapp'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  LecleSocialShare.M.shareFileToMessenger(
+                    filePath: image!.path,
+                    fileProviderPath: '',
+                    fileType: AssetType.image,
+                  );
+                },
+                child: const Text('Share media content file to Messenger'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  LecleSocialShare.I.shareFileToInstagram(
+                    filePath: image!.path,
+                    fileProviderPath: '',
+                    fileType: AssetType.image,
+                  );
+                },
+                child: const Text('Share media content file to Instagram'),
+              ),
+            ],
+          ),
+        ));
+  }
+}
